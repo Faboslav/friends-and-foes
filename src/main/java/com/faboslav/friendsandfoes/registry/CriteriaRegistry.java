c66767f772fb4d2918b406700d3d75ef6df88667
Aleš Fabiánek <fabianek.ales@gmail.com>
2021-12-30 21:47:35 +0100

    Added non-working tame_glare criteria

---
+++ src/main/java/com/faboslav/friendsandfoes/registry/CriteriaRegistry.java
@@ -0,0 +1,23 @@
+package com.faboslav.friendsandfoes.registry;
+
+import com.faboslav.friendsandfoes.advancements.criterion.TameGlareCriterion;
+import net.fabricmc.fabric.api.object.builder.v1.advancement.CriterionRegistry;
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.advancement.criterion.Criterion;
+
+/**
+ * @see Criteria
+ */
+public class CriteriaRegistry
+{
+	public static final TameGlareCriterion TAME_GLARE = register(new TameGlareCriterion());
+
+	private static <T extends Criterion<?>> T register(T criterion) {
+		return CriterionRegistry.register(criterion);
+	}
+
+	public static void init() {
+		System.out.println("criteria init");
+		System.out.println(Criteria.getCriteria());
+	}
+}

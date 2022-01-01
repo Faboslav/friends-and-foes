b7fbdcde4f661d16daace426bb2ca0f688b91176
Aleš Fabiánek <fabianek.ales@gmail.com>
2022-01-01 20:34:38 +0100

    Corrected glare navigation

--- src/main/java/com/faboslav/friendsandfoes/entity/ai/pathing/GlareNavigation.java
+++ src/main/java/com/faboslav/friendsandfoes/entity/ai/pathing/GlareNavigation.java
@@ -4,49 +4,56 @@ import com.faboslav.friendsandfoes.entity.passive.GlareEntity;
 import net.minecraft.entity.ai.pathing.BirdNavigation;
 import net.minecraft.entity.ai.pathing.PathNode;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.LightType;
 import net.minecraft.world.World;
 
 public class GlareNavigation extends BirdNavigation
 {
-	GlareEntity glare;
+	int lastPathNodeBaseLightLevel = 1;
 
 	public GlareNavigation(GlareEntity glare, World world) {
 		super(glare, world);
-
-		this.glare = glare;
 	}
 
 	protected void adjustPath() {
 		super.adjustPath();
 
+		GlareEntity glare = (GlareEntity) this.entity;
+		World world = this.world;
+		boolean isGlareTamed = glare.isTamed();
+		boolean isSkyVisible = world.isSkyVisible(
+			new BlockPos(
+				glare.getX(),
+				glare.getY() + 0.5D,
+				glare.getZ()
+			)
+		);
+		int glareBlockPosBaseLightLevel = world.getBaseLightLevel(glare.getBlockPos(), 0);
+		boolean isGlareInDarkSpot = glareBlockPosBaseLightLevel == 0;
+
+		this.lastPathNodeBaseLightLevel = glareBlockPosBaseLightLevel;
+
 		for (int i = 0; i < this.currentPath.getLength(); ++i) {
 			PathNode pathNode = this.currentPath.getNode(i);
+			BlockPos pathNodeBlockPos = new BlockPos(pathNode.x, pathNode.y, pathNode.z);
+			int pathNodeBaseLightLevel = world.getBaseLightLevel(pathNodeBlockPos, 0);
 
-			boolean isGlareTamed = this.glare.isTamed();
-			boolean isSkyVisible = this.world.isSkyVisible(
-				new BlockPos(
-					this.entity.getX(),
-					this.entity.getY() + 0.5D,
-					this.entity.getZ()
-				)
-			);
-			boolean isBlockBlockLightLevelDark = this.world.getLightLevel(
-				LightType.BLOCK,
-				new BlockPos(pathNode.x, pathNode.y, pathNode.z)
-			) == 0;
-			boolean isBlockSkyLevelDark = this.world.getLightLevel(
-				LightType.SKY,
-				new BlockPos(pathNode.x, pathNode.y, pathNode.z)
-			) == 0;
+			if (isGlareTamed || isSkyVisible) {
+				continue;
+			}
 
 			if (
-				!isGlareTamed
-				&& !isSkyVisible
-				&& isBlockBlockLightLevelDark
-				&& isBlockSkyLevelDark
+				isGlareInDarkSpot
+				&& pathNodeBaseLightLevel >= this.lastPathNodeBaseLightLevel
 			) {
+				this.lastPathNodeBaseLightLevel = pathNodeBaseLightLevel;
+				continue;
+			}
+
+			boolean pathNodeIsDarkSpot = pathNodeBaseLightLevel == 0;
+
+			if (pathNodeIsDarkSpot) {
 				this.currentPath.setLength(i);
+
 				return;
 			}
 		}

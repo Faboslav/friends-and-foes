11d24dab77c0b1d4591ecd5ffbbf6f70e55a3c59
Aleš Fabiánek <fabianek.ales@gmail.com>
2022-05-27 16:16:37 +0200

    Reformatted code

--- common/src/main/java/com/faboslav/friendsandfoes/entity/AnimatedEntity.java
+++ common/src/main/java/com/faboslav/friendsandfoes/entity/AnimatedEntity.java
@@ -1,8 +1,6 @@
 package com.faboslav.friendsandfoes.entity;
 
 import com.faboslav.friendsandfoes.client.render.entity.animation.AnimationContextTracker;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 
 public interface AnimatedEntity
 {

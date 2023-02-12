e8242c8d3b63503c2eb686ac3b7152c3ff804901
Aleš Fabiánek <fabianek.ales@gmail.com>
2023-02-12 10:32:18 +0100

    Goal debug

--- common/src/main/java/com/faboslav/friendsandfoes/entity/ai/brain/task/TuffGolemSleepTask.java
+++ common/src/main/java/com/faboslav/friendsandfoes/entity/ai/brain/task/TuffGolemSleepTask.java
@@ -8,19 +8,22 @@ import com.faboslav.friendsandfoes.init.FriendsAndFoesMemoryModuleTypes;
 import com.faboslav.friendsandfoes.util.RandomGenerator;
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.entity.ai.brain.MemoryModuleState;
+import net.minecraft.entity.ai.brain.MemoryModuleType;
 import net.minecraft.entity.ai.brain.task.Task;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Vec3d;
 
 public class TuffGolemSleepTask extends Task<TuffGolemEntity>
 {
-	private final static int MIN_TICKS_TO_SLEEP = 1200;
-	private final static int MAX_TICKS_TO_SLEEP = 3600;
-	private int ticksToSleep;
+	private final static int MIN_TICKS_TO_SLEEP = 200;
+	//private final static int MIN_TICKS_TO_SLEEP = 1200;
+	private final static int MAX_TICKS_TO_SLEEP = 400;
+	//private final static int MAX_TICKS_TO_SLEEP = 3600;
 
 	public TuffGolemSleepTask() {
 		super(ImmutableMap.of(
 			FriendsAndFoesMemoryModuleTypes.TUFF_GOLEM_SLEEP_COOLDOWN.get(), MemoryModuleState.VALUE_ABSENT
-		), MAX_TICKS_TO_SLEEP);
+		), MIN_TICKS_TO_SLEEP, MAX_TICKS_TO_SLEEP);
 	}
 
 	@Override
@@ -28,7 +31,7 @@ public class TuffGolemSleepTask extends Task<TuffGolemEntity>
 		ServerWorld world,
 		TuffGolemEntity tuffGolem
 	) {
-		if (tuffGolem.isAtHome() == false) {
+		if (tuffGolem.isAtHomePos() == false) {
 			return false;
 		}
 
@@ -42,10 +45,34 @@ public class TuffGolemSleepTask extends Task<TuffGolemEntity>
 		TuffGolemEntity tuffGolem,
 		long time
 	) {
-		FriendsAndFoes.getLogger().info("start sleeping");
-		this.ticksToSleep = RandomGenerator.generateInt(MIN_TICKS_TO_SLEEP, MAX_TICKS_TO_SLEEP);
+		tuffGolem.getBrain().forget(MemoryModuleType.LOOK_TARGET);
+		tuffGolem.getBrain().forget(MemoryModuleType.WALK_TARGET);
+
+		tuffGolem.setSpawnYaw(tuffGolem.getHomeYaw());
+
+		tuffGolem.getNavigation().setSpeed(0);
 		tuffGolem.getNavigation().stop();
-		tuffGolem.startSleeping();
+		tuffGolem.getMoveControl().moveTo(tuffGolem.getX(), tuffGolem.getY(), tuffGolem.getZ(), 0);
+		tuffGolem.getMoveControl().tick();
+		tuffGolem.getLookControl().lookAt(tuffGolem.getLookControl().getLookX(), tuffGolem.getLookControl().getLookY(), tuffGolem.getLookControl().getLookZ());
+		tuffGolem.getLookControl().tick();
+
+		tuffGolem.setJumping(false);
+		tuffGolem.setMovementSpeed(0.0F);
+		tuffGolem.prevHorizontalSpeed = 0.0F;
+		tuffGolem.horizontalSpeed = 0.0F;
+		tuffGolem.sidewaysSpeed = 0.0F;
+		tuffGolem.upwardSpeed = 0.0F;
+		tuffGolem.setVelocity(Vec3d.ZERO);
+		tuffGolem.velocityDirty = true;
+
+		if(tuffGolem.isInPose(TuffGolemEntityPose.STANDING.get())) {
+			FriendsAndFoes.getLogger().info("start startSleeping");
+			tuffGolem.startSleeping();
+		} else if(tuffGolem.isInPose(TuffGolemEntityPose.STANDING_WITH_ITEM.get())) {
+			FriendsAndFoes.getLogger().info("start startSleepingWithItem");
+			tuffGolem.startSleepingWithItem();
+		}
 	}
 
 	@Override
@@ -54,18 +81,7 @@ public class TuffGolemSleepTask extends Task<TuffGolemEntity>
 		TuffGolemEntity tuffGolem,
 		long time
 	) {
-		return this.ticksToSleep > 0;
-	}
-
-	@Override
-	protected void keepRunning(
-		ServerWorld serverWorld,
-		TuffGolemEntity tuffGolem,
-		long l
-	) {
-		if(this.ticksToSleep > 0) {
-			this.ticksToSleep -= 1;
-		}
+		return true;
 	}
 
 	@Override
@@ -76,6 +92,11 @@ public class TuffGolemSleepTask extends Task<TuffGolemEntity>
 	) {
 		FriendsAndFoes.getLogger().info("stop sleeping");
 		TuffGolemBrain.setSleepCooldown(tuffGolem);
-		tuffGolem.startStanding();
+
+		if(tuffGolem.isInPose(TuffGolemEntityPose.SLEEPING.get())) {
+			tuffGolem.startStanding();
+		} else if(tuffGolem.isInPose(TuffGolemEntityPose.SLEEPING_WITH_ITEM.get())) {
+			tuffGolem.startStandingWithItem();
+		}
 	}
 }
---
+++ common/src/main/java/com/faboslav/friendsandfoes/entity/ai/brain/task/TuffGolemWalkTask.java
@@ -0,0 +1,49 @@
+package com.faboslav.friendsandfoes.entity.ai.brain.task;
+
+import com.faboslav.friendsandfoes.entity.TuffGolemEntity;
+import com.faboslav.friendsandfoes.entity.pose.TuffGolemEntityPose;
+import net.minecraft.entity.ai.brain.task.WalkTask;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.server.world.ServerWorld;
+
+public final class TuffGolemWalkTask extends WalkTask
+{
+	public TuffGolemWalkTask(float speed) {
+		super(speed);
+	}
+
+	@Override
+	protected boolean shouldRun(
+		ServerWorld world,
+		PathAwareEntity pathAwareEntity
+	) {
+		TuffGolemEntity tuffGolem = (TuffGolemEntity) pathAwareEntity;
+
+		if(
+			tuffGolem.isInSleepingPose()
+			|| tuffGolem.isGlued()
+			|| tuffGolem.isLeashed()
+			|| tuffGolem.hasVehicle()
+		) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	protected void run(
+		ServerWorld serverWorld,
+		PathAwareEntity pathAwareEntity,
+		long l
+	) {
+		TuffGolemEntity tuffGolem = (TuffGolemEntity) pathAwareEntity;
+		if(tuffGolem.wasInPose(TuffGolemEntityPose.SLEEPING.get())) {
+			tuffGolem.startStanding();
+		} else if(tuffGolem.wasInPose(TuffGolemEntityPose.SLEEPING_WITH_ITEM.get())) {
+			tuffGolem.startStandingWithItem();
+		}
+
+		super.run(serverWorld, tuffGolem, l);
+	}
+}

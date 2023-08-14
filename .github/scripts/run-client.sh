06995a370b515aedbf138be28cf76aaa39bdb17e
Faboslav <fabianek.ales@gmail.com>
2023-08-14 23:02:00 +0200

    wip

--- .github/scripts/run-client.sh
+++ .github/scripts/run-client.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-./gradlew $1:runClient > gradle_output.txt 2>&1 &
+./gradlew $1:runClient > gradle_client_output.txt 2>&1 &
 
-TARGET_PATTERN='minecraft:textures/atlas/mob_effects\.png-atlas'
-TIMEOUT=600
+SUCCESS_PATTERN='minecraft:textures/atlas/mob_effects\.png-atlas'
+ERROR_PATTERN='Execution failed for task'
+TIMEOUT=1800
 ELAPSED=0
 
 while [ $ELAPSED -lt $TIMEOUT ]; do
-    if grep -Eq "$TARGET_PATTERN" gradle_output.txt; then
+    if grep -Eq "$SUCCESS_PATTERN" gradle_client_output.txt; then
         pkill -P $$
         exit 0
+    fi
+
+    if grep -Eq "$ERROR_PATTERN" gradle_client_output.txt; then
+        pkill -P $$
+        exit 1
     fi
 
     sleep 1
--- .github/scripts/run-server.sh
+++ .github/scripts/run-server.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-./gradlew $1:runServer --args="nogui" > gradle_output.txt 2>&1 &
+./gradlew $1:runServer --args="nogui" > gradle_server_output.txt 2>&1 &
 
-TARGET_PATTERN='For help, type "help"'
-TIMEOUT=600
+SUCCESS_PATTERN='For help, type "help"'
+ERROR_PATTERN='Execution failed for task'
+TIMEOUT=1800
 ELAPSED=0
 
 while [ $ELAPSED -lt $TIMEOUT ]; do
-    if grep -Eq "$TARGET_PATTERN" gradle_output.txt; then
+    if grep -Eq "$SUCCESS_PATTERN" gradle_server_output.txt; then
         pkill -P $$
         exit 0
+    fi
+
+    if grep -Eq "$ERROR_PATTERN" gradle_server_output.txt; then
+        pkill -P $$
+        exit 1
     fi
 
     sleep 1

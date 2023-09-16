d221a5ab4c210f74876a6b9081d18081371e7bd8
Faboslav <fabianek.ales@gmail.com>
2023-09-16 11:09:16 +0200

    Added missing scripts

---
+++ .github/scripts/run-server.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+./gradlew $1:runServer --args="nogui" > gradle_server_output.txt 2>&1 &
+
+SUCCESS_PATTERN='For help, type "help"'
+ERROR_PATTERNS=(
+    'For more details see the full crash report file'
+    ' end of report '
+)
+TIMEOUT=1800
+ELAPSED=0
+
+while [ $ELAPSED -lt $TIMEOUT ]; do
+    if grep -Eq "$SUCCESS_PATTERN" gradle_server_output.txt; then
+        pkill -P $$
+        exit 0
+    fi
+
+    for ERROR_PATTERN in "${ERROR_PATTERNS[@]}"; do
+        if grep -Eq "$ERROR_PATTERN" gradle_server_output.txt; then
+            pkill -P $$
+            exit 1
+        fi
+    done
+
+    sleep 1
+    ELAPSED=$((ELAPSED + 1))
+done
+
+if [ $ELAPSED -ge $TIMEOUT ]; then
+    exit 1
+fi

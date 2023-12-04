6c4a040ff0cb872ef5cb84b358cdced4aefcc846
Aleš Fabiánek <fabianek.ales@gmail.com>
2023-12-04 16:45:31 +0100

    supress

--- fabric/src/main/java/com/faboslav/friendsandfoes/platform/fabric/RegistryHelperImpl.java
+++ fabric/src/main/java/com/faboslav/friendsandfoes/platform/fabric/RegistryHelperImpl.java
@@ -36,7 +36,7 @@ import net.minecraft.world.poi.PointOfInterestType;
 
 import java.util.function.Supplier;
 
-@SuppressWarnings("unsafe")
+@SuppressWarnings({"unsafe", "unchecked"})
 public final class RegistryHelperImpl
 {
 	public static void addToItemGroupBefore(ItemGroup itemGroup, Item item, Item before) {

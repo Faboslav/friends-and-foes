c4ffff9232f55a8c6ff0428d5e553508c024f68e
Aleš Fabiánek <fabianek.ales@gmail.com>
2024-09-16 09:53:26 +0200

    CI test

--- .github/scripts/old/generate-run-matrix.sh
+++ .github/scripts/old/generate-run-matrix.sh
@@ -5,9 +5,7 @@ enabled_platforms=$(awk -F= '/enabled_platforms/{print $2}' gradle.properties | 
 version=$(awk -F= '/minecraft_version/{print $2; exit}' gradle.properties | tr -d ' ')
 
 for platform in $(echo $enabled_platforms | tr ',' ' '); do
-  matrix_entry="{\"mod_loader\":\"$platform\",\"version\":\"$version\",\"script\":\"client\"},"
-  matrix_content+="$matrix_entry"
-  matrix_entry="{\"mod_loader\":\"$platform\",\"version\":\"$version\",\"script\":\"server\"},"
+  matrix_entry="{\"mod_loader\":\"$platform\",\"version\":\"$version\"},"
   matrix_content+="$matrix_entry"
 done
 

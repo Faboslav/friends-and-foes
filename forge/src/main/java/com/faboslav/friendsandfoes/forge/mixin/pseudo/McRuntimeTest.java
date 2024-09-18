dcec260d0c57d119e999ea99e694860196aa855d
Faboslav <fabianek.ales@gmail.com>
2024-09-18 08:34:48 +0200

    CI test

---
+++ forge/src/main/java/com/faboslav/friendsandfoes/forge/mixin/pseudo/McRuntimeTest.java
@@ -0,0 +1,23 @@
+package com.faboslav.friendsandfoes.forge.mixin.pseudo;
+
+import net.minecraft.world.level.storage.LevelSummary;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LevelSummary.class)
+public class McRuntimeTest
+{
+	@Inject(method = "isExperimental", at = @At(value = "RETURN"), cancellable = true)
+	public void friendsandfoes$isExperimental(CallbackInfoReturnable<Boolean> cir) {
+		if(friendsandfoes$isClassAvailable("me.earth.mc_runtime_test.McRuntimeTest")) {
+			cir.setReturnValue(false);
+		}
+	}
+
+	private boolean friendsandfoes$isClassAvailable(String className) {
+		String classPath = className.replace('.', '/') + ".class";
+		return getClass().getClassLoader().getResource(classPath) != null;
+	}
+}

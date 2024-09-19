44d0b0cc14de3ff7ba6501af4e7d460f85756a62
Faboslav <fabianek.ales@gmail.com>
2024-09-19 14:45:25 +0200

    fixed import

--- common/src/main/java/com/faboslav/friendsandfoes/common/init/FriendsAndFoesBlockSetTypes.java
+++ common/src/main/java/com/faboslav/friendsandfoes/common/init/FriendsAndFoesBlockSetTypes.java
@@ -1,6 +1,6 @@
 package com.faboslav.friendsandfoes.common.init;
 
-import com.faboslav.friendsandfoes.FriendsAndFoes;
+import com.faboslav.friendsandfoes.common.FriendsAndFoes;
 import com.faboslav.friendsandfoes.common.events.block.RegisterBlockSetTypeEvent;
 import net.minecraft.block.BlockSetType;
 import net.minecraft.sound.BlockSoundGroup;

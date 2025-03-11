af09e9de492a2ef7c8ff97013cd8b0202ed26b3e
Faboslav <fabianek.ales@gmail.com>
2025-03-11 10:18:29 +0100

    updated CI

--- .github/scripts/generate-run-matrix.sh
+++ .github/scripts/generate-run-matrix.sh
@@ -4,16 +4,9 @@ matrix_content="{\"include\":["
 enabled_platforms=$(awk -F= '/stonecutter_enabled_platforms/{print $2}' gradle.properties | tr -d ' ')
 
 for platform in $(echo $enabled_platforms | tr ',' ' '); do
-  if [[ "$platform" == "forge" ]]; then
-    mod_loader_alias="lexforge"
-  else
-    mod_loader_alias="$platform"
-  fi
-
   versions=$(awk -F= '/stonecutter_enabled_'$platform'_versions/{print $2}' gradle.properties | tr -d ' ')
   for version in $(echo $versions | tr ',' ' '); do
-    yacl_version=$(awk -F= '/yacl_version/{print $2}' versions/"${version}"/gradle.properties | tr -d ' ')
-    matrix_entry="{\"mod_loader\":\"$platform\",\"mod_loader_alias\":\"$mod_loader_alias\",\"version\":\"$version\",\"yacl_version\":\"$yacl_version\"},"
+    matrix_entry="{\"mod_loader\":\"$platform\",\"version\":\"$version\"},"
     matrix_content+="$matrix_entry"
   done
 done

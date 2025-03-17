429910619f7ddd3c3dac34cb89bb2d30dfec931f
Faboslav <fabianek.ales@gmail.com>
2025-03-17 21:32:45 +0100

    Fixed file

--- common/src/main/java/com/faboslav/friendsandfoes/common/client/render/entity/renderer/MoobloomEntityRenderer.java
+++ common/src/main/java/com/faboslav/friendsandfoes/common/client/render/entity/renderer/MoobloomEntityRenderer.java
@@ -11,33 +11,32 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
 //? >=1.21.3 {
-
 import net.minecraft.client.renderer.entity.AgeableMobRenderer;
 import com.faboslav.friendsandfoes.common.client.render.entity.state.MoobloomRenderState;
 //?} else {
 /*import net.minecraft.client.renderer.entity.MobRenderer;
-//}
+*///?}
 
 @Environment(EnvType.CLIENT)
 @SuppressWarnings({"rawtypes", "unchecked"})
 //? >=1.21.3 {
 public final class MoobloomEntityRenderer extends AgeableMobRenderer<MoobloomEntity, MoobloomRenderState, CowModel>
 //?} else {
-/^public final class MoobloomEntityRenderer extends MobRenderer<MoobloomEntity, CowModel<MoobloomEntity>>
-^///?}
+/*public final class MoobloomEntityRenderer extends MobRenderer<MoobloomEntity, CowModel<MoobloomEntity>>
+*///?}
 {
 	public MoobloomEntityRenderer(EntityRendererProvider.Context context) {
 		//? >=1.21.3 {
 		super(context, new CowModel(context.bakeLayer(FriendsAndFoesEntityModelLayers.MOOBLOOM_LAYER)),  new CowModel(context.bakeLayer(FriendsAndFoesEntityModelLayers.MOOBLOOM_BABY_LAYER)), 0.7F);
 		//?} else {
-		/^super(context, new CowModel(context.bakeLayer(FriendsAndFoesEntityModelLayers.MOOBLOOM_LAYER)), 0.7F);
-		^///?}
+		/*super(context, new CowModel(context.bakeLayer(FriendsAndFoesEntityModelLayers.MOOBLOOM_LAYER)), 0.7F);
+		*///?}
 
 		//? >=1.21.3 {
 		this.addLayer(new MoobloomFlowerFeatureRenderer(this, context.getBlockRenderDispatcher()));
 		//?} else {
-		/^this.addLayer(new MoobloomFlowerFeatureRenderer(this));
-		^///?}
+		/*this.addLayer(new MoobloomFlowerFeatureRenderer(this));
+		*///?}
 	}
 
 	//? >=1.21.3 {
@@ -57,8 +56,8 @@ public final class MoobloomEntityRenderer extends AgeableMobRenderer<MoobloomEnt
 	//? >=1.21.3 {
 	public ResourceLocation getTextureLocation(MoobloomRenderState moobloomRenderState)
 	//?} else {
-	/^public ResourceLocation getTextureLocation(MoobloomEntity moobloom)
-	^///?}
+	/*public ResourceLocation getTextureLocation(MoobloomEntity moobloom)
+	*///?}
 	{
 		//? >=1.21.3 {
 		var moobloom = moobloomRenderState.moobloom;
@@ -66,4 +65,4 @@ public final class MoobloomEntityRenderer extends AgeableMobRenderer<MoobloomEnt
 
 		return FriendsAndFoes.makeID("textures/entity/moobloom/moobloom_" + moobloom.getVariant().getName() + ".png");
 	}
-}*/
+}

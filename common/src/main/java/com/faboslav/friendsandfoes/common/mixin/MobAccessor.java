c3b59759f7d51e21adbd8ed430cf5aeaa152c827
Faboslav <fabianek.ales@gmail.com>
2025-04-03 15:52:32 +0200

    Fixed mixin

--- common/src/main/java/com/faboslav/friendsandfoes/common/mixin/MobAccessor.java
+++ common/src/main/java/com/faboslav/friendsandfoes/common/mixin/MobAccessor.java
@@ -2,7 +2,7 @@ package com.faboslav.friendsandfoes.common.mixin;
 
 import net.minecraft.world.entity.Mob;
 
-//? <=1.21.5 {
+//? <=1.21.4 {
 import net.minecraft.world.entity.EquipmentSlot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(Mob.class)
 public interface MobAccessor
 {
-	//? <=1.21.5 {
+	//? <=1.21.4 {
 	@Invoker("getEquipmentDropChance")
 	float friendsandfoes$getEquipmentDropChance(EquipmentSlot slot);
 	//?}

0a74af8d1fcb89520df64fc9276a216ca98a1e25
Faboslav <fabianek.ales@gmail.com>
2025-04-10 21:18:38 +0200

    Fixed import

--- common/src/main/java/com/faboslav/friendsandfoes/common/mixin/PatrolSpawnerMixin.java
+++ common/src/main/java/com/faboslav/friendsandfoes/common/mixin/PatrolSpawnerMixin.java
@@ -15,6 +15,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+
 //? >=1.21.5 {
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 //?}
@@ -24,6 +25,7 @@ import net.minecraft.world.entity.EntitySpawnReason;
 import com.faboslav.friendsandfoes.common.versions.VersionedEntitySpawnReason;
 //?} else {
 /*import net.minecraft.world.entity.MobSpawnType;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 *///?}
 
 @Mixin(PatrolSpawner.class)

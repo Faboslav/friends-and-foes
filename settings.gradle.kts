aa0899673f06cd8d5163b4a7381f8a7fe7b97956
Faboslav <fabianek.ales@gmail.com>
2025-04-11 18:33:56 +0200

    stonecutter bump

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.kikugie.stonecutter") version "0.6-beta.1"
+    id("dev.kikugie.stonecutter") version "0.6-beta.2"
 	id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
@@ -27,6 +27,7 @@ val uniqueVersions = dists.values.flatten().distinct()
 
 stonecutter {
 	kotlinController = true
+	centralScript = "build.gradle.kts"
 
 	create(rootProject) {
 		versions(*uniqueVersions.toTypedArray())

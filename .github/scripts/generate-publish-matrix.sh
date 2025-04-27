52a45be534ffc7128031a8a239f65ff671914ec6
Faboslav <fabianek.ales@gmail.com>
2025-04-27 12:53:07 +0200

    CI corrections

--- .github/scripts/generate-publish-matrix.sh
+++ .github/scripts/generate-publish-matrix.sh
@@ -2,6 +2,10 @@
 
 allowed_mod_loaders=$1
 allowed_versions=$2
+curseforge_fabric_project_id=$3
+curseforge_forge_project_id=$4
+modrinth_fabric_project_id=$5
+modrinth_forge_project_id=$6
 
 echo ${allowed_mod_loaders};
 echo ${allowed_versions};
@@ -19,11 +23,15 @@ for platform in $(echo $enabled_platforms | tr ',' ' '); do
       if [[ " ${allowed_versions_array[@]} " =~ " ${version} " ]]; then
         if [[ "$platform" == "fabric" ]]; then
           supported_mod_loaders="\"fabric\",\"quilt\""
+          curseforge_project_id="$curseforge_fabric_project_id"
+          modrinth_project_id="$modrinth_fabric_project_id"
         else
           supported_mod_loaders="\"$platform\""
+          curseforge_project_id="$curseforge_forge_project_id"
+          modrinth_project_id="$modrinth_forge_project_id"
         fi
 
-        matrix_entry="{\"mod_loader\":\"$platform\",\"version\":\"$version\",\"supported_mod_loaders\":[$supported_mod_loaders]},"
+        matrix_entry="{\"mod_loader\":\"$platform\",\"version\":\"$version\",\"supported_mod_loaders\":[$supported_mod_loaders]},\"curseforge_project_id\":\"$curseforge_project_id\",\"modrinth_project_id\":\"$modrinth_project_id\"},"
         matrix_content+="$matrix_entry"
       fi
     done

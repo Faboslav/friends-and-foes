4923c570554bcb0f8a2b21a44dea9e3577d8890f
Faboslav <fabianek.ales@gmail.com>
2025-10-17 09:14:02 +0200

    gradle tests

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -3,7 +3,8 @@ val IS_CI = System.getenv("CI") == "true"
 plugins {
 	id("dev.kikugie.stonecutter")
 	id("net.neoforged.moddev") version "2.0.115" apply false
-	id("fabric-loom") version "1.12.0-alpha.+" apply false
+	id("net.neoforged.moddev") version "2.0.107" apply false
+	id("fabric-loom") version "1.11-SNAPSHOT" apply false
 }
 
 if (IS_CI) stonecutter active null
